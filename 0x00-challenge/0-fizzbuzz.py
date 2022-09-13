7f78bfbd1ea93ebc0990ceb7565c1da06ed66b02
Same Titan <49342907+loopopski@users.noreply.github.com>
2022-09-13 03:33:35 +0100

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -23,7 +23,6 @@ def fizzbuzz(n):
             tmp_result.append("Fizz")
         elif (i % 5) == 0:
                 tmp_result.append("Buzz")
-       
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
